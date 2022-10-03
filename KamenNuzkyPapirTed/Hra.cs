064fa6c418a1c20d45af55e49a81b2b5c9990428
Hénková Monika <Henkova.Monika@stud.vassboskovice.cz>
2022-10-03 13:46:58 +0200

    Přidejte soubory projektu.

---
+++ KamenNuzkyPapirTed/Hra.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+
+namespace KamenNuzkyPapirTed
+{
+    public class Hra
+    {
+        private Timer timer;
+        public Hrac Hrac1 { get; }
+        public Hrac Hrac2 { get; }
+
+        public Hra(int money)
+        {
+            Hrac1 = new Hrac(money);
+            Hrac2 = new Hrac(money);
+            timer = new Timer(1000);
+            timer.Elapsed += Timer_Elapsed;
+        }
+        
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var p1 = Hrac1.GetState();
+            var p2 = Hrac2.GetState();
+
+            if(p1 != p2)
+            {
+               if (p1 == Hrac.PlayState.Kámen)
+               {
+
+               }
+               else if (p2 == Hrac.PlayState.Nůžky)
+               {
+
+               }
+               else
+               {
+
+               }     
+            }
+        }
+
+    }
+}
---
+++ KamenNuzkyPapirTed/Hrac.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KamenNuzkyPapirTed
+{
+    public class Hrac
+    {
+        private Random random = new Random();
+        public int Money { get; internal set; }
+
+        public Hrac (int money)
+        {
+            Money = money;
+        }
+        public PlayState GetState()
+        {
+            int value = random.Next(0, 3);
+            return (PlayState)value;
+       
+        }
+        public enum PlayState
+        {
+            Kámen,
+            Nůžky,
+            Papír
+        }
+
+        public void Win()
+        {
+            Money++;
+        }
+
+        public void Lose()
+        {
+            Money--;
+        }
+    
+    
+    }
+
+    
+
+
+
+}
