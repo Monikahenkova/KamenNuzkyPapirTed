78f4627bded6e1663389644ad3152e95db338130
Monika Hénková <Henkova.Monika@stud.vassboskovice.cz>
2022-10-10 13:44:58 +0200

    zzt

---
+++ KamenNuzkyPapirTed/figurka.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KamenNuzkyPapirTed
+{
+    internal class figurka
+    {
+        public figurkaColor Color { get; }
+
+        public figurka (FigurkaColor color)
+        {
+            figurka = new figurka[4];
+            for (int i = 0; i < Figures.Lenght; i++)
+        }
+    }
+
+}
